5b9793fe32df7b794e2a6b99173585baecd4283e
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-07-16 21:45:44 -0300

    c

---
+++ AsyncStorage/App.js
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState } from 'react';
+
+export default function App() {
+  const [user, setUser ] = useState ('');
+  const [consulta,setC1onsulta] = useState ('');
+
+  //salvar usuario
+  
+try {
+    async function userSalve() {
+      await AsyncStorage.setItem('@id_user', user);
+      alert ('salvo com sucesso')
+  }
+  
+} catch (error) {
+  alert( 'erro ao salvar')
+}
+
+    
+  
+  return (
+    <View style={styles.container}>
+      <Text>Usuario</Text>
+      <TextInput style={styles.input}
+      placeholder='digite o nome do usuario'
+      value={user}
+      onChangeText={setUser}
+      />
+      <Button
+      title='Salvar'
+      onPress={userSalve}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input : {
+    width : 200,
+    backgroundColor : '#fefefe',
+
+  }
+});
