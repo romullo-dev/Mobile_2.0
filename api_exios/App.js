650ac912a15652f6132cb35c688c51782b0e1776
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-07-23 21:39:11 -0300

    d

---
+++ api_exios/App.js
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import axios from 'axios'
+import { useState } from 'react';
+
+export default function App() {
+  const [autor, setAutor] = useState([]);
+
+ 
+ async function getAutor() {
+    try {
+      const resposta = await axios.get('http://10.0.2.2:81/livros/api.php');
+      setAutor (resposta.data);
+    } catch (error) {
+      alert ('erro');
+    }
+ }
+ 
+  return (
+    <View style={styles.container}>
+      <Text>API Axios</Text>
+      <Button title='Mostrar autor' onPress={getAutor} />
+       <FlatList style={styles.flat} 
+       data={autor}
+       keyExtractor={(item)=> item.id}
+       renderItem={({ item }) => (
+        <View>
+          <Text>{item.nome}</Text>
+        </View>
+      )}
+       />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop : 200,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  flat : {
+    paddingTop : 200,
+    flex : 1,
+    color: '#fff',
+
+
+
+  }
+});
